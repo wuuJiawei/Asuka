5730364435a451309d3e57d63bbed2889f04a61d
wujiawei <wujiawei0926@yeah.net>
2020-07-16 18:03:21 +0800

    尝试动态重载principal

--- src/main/java/com/asuka/common/security/SecurityUtils.java
+++ src/main/java/com/asuka/common/security/SecurityUtils.java
@@ -1,7 +1,17 @@
 package com.asuka.common.security;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+
+import javax.management.relation.RoleList;
+import java.util.Collection;
+import java.util.Set;
 
 /**
  * @author wujiawei0926@yeah.net
@@ -14,11 +24,16 @@ public class SecurityUtils {
      * 获取用户账户
      **/
     public static String getUsername() {
-        try {
-            return getLoginUser().getUsername();
-        } catch (Exception e) {
-            return "";
-        }
+        return getLoginUser().getUsername();
+    }
+
+    /**
+     * 获取权限
+     *
+     * @return
+     */
+    public static Set<String> getPermissions() {
+        return getLoginUser().getPermissions();
     }
 
     /**
@@ -28,7 +43,7 @@ public class SecurityUtils {
         try {
             return (SecurityUser) getAuthentication().getPrincipal();
         } catch (Exception e) {
-            return null;
+            return new SecurityUser();
         }
     }
 
@@ -39,5 +54,40 @@ public class SecurityUtils {
         return SecurityContextHolder.getContext().getAuthentication();
     }
 
+    public static void reloadUsernameOfLoginUser(String username) {
+        reloadLoginUser(username, null, null, null);
+    }
+
+    public static void reloadPasswordOfLoginUser(String password) {
+        reloadLoginUser(null, password, null, null);
+    }
+
+    public static void reloadPermissionOfLoginUser(Set<String> permissions) {
+        reloadLoginUser(null, null, null, permissions);
+    }
+
+    public static void reloadRoleOfLoginUser(Set<String> roles) {
+        reloadLoginUser(null, null, roles, null);
+    }
+
+    public static void reloadLoginUser(String username, String password, Set<String> roleSet, Set<String> permissionSet) {
+        SecurityUser securityUser = getLoginUser();
+        if (securityUser == null) {
+            return;
+        }
+        username = StringUtils.isEmpty(username) ? securityUser.getUsername() : username;
+        password = StringUtils.isEmpty(password) ? securityUser.getPassword() : password;
+        roleSet = CollectionUtils.isEmpty(roleSet) ? securityUser.getRoles() : roleSet;
+        permissionSet = CollectionUtils.isEmpty(permissionSet) ? securityUser.getPermissions() : permissionSet;
+
+        securityUser.setPermissions(permissionSet);
+        securityUser.setRoles(roleSet);
+        securityUser.setUsername(username);
+        securityUser.setPassword(password);
+
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
+        token.setDetails(securityUser);
+        SecurityContextHolder.getContext().setAuthentication(token);
+    }
 
 }
